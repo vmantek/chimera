6416a5db73e74f56d8f4179bca5473b286ad282a
Victor Salaman <vsalaman@vmantek.com>
2022-10-03 18:05:11 -0400

    Allows for disabling of deployer

--- src/main/java/com/vmantek/chimera/q2/EnableQ2.java
+++ src/main/java/com/vmantek/chimera/q2/EnableQ2.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -12,5 +13,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Import(Q2Configuration.class)
 @Documented
+@Inherited
 public @interface EnableQ2 {
+    String deployDirectory() default "deploy";
+    boolean enableDeployer() default true;
 }
--- src/main/java/com/vmantek/chimera/q2/Q2Configuration.java
+++ src/main/java/com/vmantek/chimera/q2/Q2Configuration.java
@@ -1,33 +1,32 @@
 package com.vmantek.chimera.q2;
 
 import com.vmantek.chimera.deploy.SpringResourceDeployer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import java.io.File;
+import java.util.Map;
 
 @Configuration
 public class Q2Configuration
 {
+    private final ApplicationArguments args;
+    private final ApplicationContext ctx;
+
     @Value("${q2.default-arguments:-r}")
     private String defaultArguments;
 
-    public String getDefaultArguments()
+    public Q2Configuration(
+        ApplicationArguments args,
+        ApplicationContext ctx)
     {
-        return defaultArguments;
-    }
-
-    @Autowired
-    private ApplicationArguments args;
-
-    public void setDefaultArguments(String defaultArguments)
-    {
-        this.defaultArguments = defaultArguments;
+        this.args = args;
+        this.ctx = ctx;
     }
 
     @Bean(initMethod = "start", destroyMethod = "stop")
@@ -46,8 +45,16 @@ public class Q2Configuration
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Q2Service q2Service(SpringResourceDeployer sysDeployer)
     {
-        File outputBase = sysDeployer.getOutputBase();
-        return new Q2Service(outputBase.getAbsolutePath(),
+        boolean enableDeployer=false;
+        String dir = "deploy";
+        Map<String, Object> beans = ctx.getBeansWithAnnotation(EnableQ2.class);
+        Object value = beans.values().stream()
+            .findFirst()
+            .orElseThrow(()->new IllegalArgumentException("@Q2Service annotation not found"));
+        EnableQ2 eq2 = value.getClass().getAnnotation(EnableQ2.class);
+        enableDeployer = eq2.enableDeployer();
+        File deployDir = enableDeployer ? sysDeployer.getOutputBase() : new File(dir);
+        return new Q2Service(deployDir.getAbsolutePath(),
                              defaultArguments,
                              args.getSourceArgs());
     }
